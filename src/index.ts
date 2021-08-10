cc5e4953124f82c3705abd9df934ad9f9b97e2be
Pham Anh Duc <88713288+SoSmoothy@users.noreply.github.com>
2021-08-10 19:57:05 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import {Manga} from "./Interface";
-import {parseHtml} from "./Util/parseHtml";
-import {getHtml} from "./Util";
+import {getHtml, parseHtml} from "./Util";
 
 export default class Api {
     /**
-     * get data
+     * Get data
      * @param {string} params - url or id of nhentai manga
      * @return {Promise<Manga>} Manga info
      */
-    static g = async (params: string):Promise<Manga> => {
-        const url = (params.startsWith("https://nhentai.net/g/")) ? params : `https://nhentai.net/g/${params}`;
-        return parseHtml(await getHtml(url), url)
+    static g = async (params: string | number):Promise<Manga> => {
+        if(typeof params == "string") {
+            const url = (params.startsWith("https://nhentai.net/g/")) ? params : `https://nhentai.net/g/${params}`;
+            return parseHtml(await getHtml(url), url);
+        } else {
+            let ID = params.toString();
+            const url = (ID.startsWith("https://nhentai.net/g/")) ? ID : `https://nhentai.net/g/${params}`;
+            return parseHtml(await getHtml(url), url);
+        }
     }
+
+    /**
+     * Convert Object to Json
+     * @param {Manga} data 
+     * @returns Json Manga
+     */
+    static toJson = (data: Manga) => {
+        return JSON.stringify(data);
+    }
+
 }
